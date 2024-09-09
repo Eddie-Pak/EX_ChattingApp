bd936f432f7769fc56af7d3b82327fa166bb9cfd
Eddie-Pak <foalstn@naver.com>
2024-09-10 01:51:33 +0900

    Login UI & Signin, Login 간단구현

---
+++ app/src/main/java/com/example/chattingapp/LoginActivity.kt
@@ -0,0 +1,63 @@
+package com.example.chattingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.chattingapp.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일 또는 패스워드가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 회원가입 성공
+                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인해주세요..", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // 회원가입 실패
+                        Toast.makeText(this, "회원가입에 실패했습니다. 다시시도해주세요..", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+        binding.logInButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일 또는 패스워드가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Log.e("LoginActivity", task.exception.toString())
+                        Toast.makeText(this, "로그인에 실패했습니다. 다시시도해주세요..", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+}
--- app/src/main/java/com/example/chattingapp/MainActivity.kt
+++ app/src/main/java/com/example/chattingapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.chattingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentUser = Firebase.auth.currentUser
+
+        if (currentUser == null) {
+            // 로그인이 안되어있음
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
